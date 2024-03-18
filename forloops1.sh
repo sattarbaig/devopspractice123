3e0a88fc5fcf2980965cd616d302585de1639ce7
sattar <sattarbaig786@gmail.com>
2024-03-18 07:11:10 +0000

    committed

---
+++ forloops1.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+DATE=$(date +%F)
+LOGSDIR=/home/centos/shellscript.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+   echo -e "error:; $R run this script with error"
+   exit 1
+fi
+
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+   echo -e "installing $2 :: $R failure $N"
+   exit 1
+else
+   echo -e "installing $2:: $G success $N"
+fi
+}
+
+
+for i in $@
+do
+    yum list installed &>>$LOGFILE
+    if [ $? -ne 0 ]
+    then
+       echo "if not install, let's install it"
+       yum install $i -y &>>$LOGFILE
+       VALIDATE $? "$i"
+    else
+       echo -e "$Y $i is successfully installed $N"
+    fi
+done

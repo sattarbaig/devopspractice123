d0240e619af0ee8e030c352563c166e2b7862768
sattar <sattarbaig786@gmail.com>
2024-04-30 14:35:20 +0530

    commit

--- forloops1.sh
+++ forloops1.sh
@@ -1,42 +1,47 @@
-#!/bin/bash
-LOGSDIR=/home/centos/shellscript.log
-DATE=$(date +%F)
-LOGFILE=$LOGSDIR/$0-DATE.log
+#!/bin/sh
+
+LOGSDIR=/tmp/$0-$DATE
 SCRIPT_NAME=$0
+DATE=$9date +%F)
 
 R="\e[31m"
 G="\e[32m"
-Y="\e[33m"
 N="\e[0m"
+Y="\e[33m"
 
 VALIDATE(){
-   if [ $1 -ne 0 ];
-   then
-      echo -e "$2 :::$R  is failure $N"
-      exit 1
-   else
-      echo -e "$2 ::: $G is success $N"
-   fi
+	if [ $1 -ne 0]
+	then
+	   echo "$2 INSTALLATION ....$R FAILURE $N"
+	   exit 1
+    else  
+	   echo "$2 INSTALLATION .... $G SUCCESS $N"
+    fi  
+
 }
 
 
+
 USERID=$(id -u)
-if [ $USERID -ne 0 ]
+if [ $USERID -ne 0]
 then
-   echo "run this script with error"
+   echo "ERROR: PLEASE run this script with error"
    exit 1
 fi
 
+
 for i in $@
 do
-    yum list installed &>>$LOGFILE
-    if [ $? -ne 0 ];
-    then
-       echo "$i is not installed, let's install it"
-       yum install $i -y &>>$LOGFILE
-       VALIDATE $? "$i"
-    else
-       echo -e "$Y $i is successfully installed"
+	yum list installed $i &>>$LOGSDIR
+	if [ $? -ne 0 ]
+	then  
+	    echo "$i is not installed... let's install it"
+		yum install $i -Y  &>> $LOGSDIR
+		VALIDATE $? "$i"
+    else 
+	    echo -e " $y $i is already installed"
     fi
+
 done
+
 
